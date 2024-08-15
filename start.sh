4bfc78f3bea1aff61d294ee34272164728d89104
Julian Mulla <jmulla@gmail.com>
2024-08-15 13:48:09 +0000

    autostart

--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
+if [ ! -f .env ]; then
+    echo $AWSENV>.env
+fi
+
+if [ ! -f fb-sa.json ]; then
+    echo $FBSA>fb-sa.json
+fi
+
+if [ ! -f firebase-auth.json ]; then
+    echo $FB>firebase-auth.json
+fi
+
 source .venv/bin/activate
 set -o allexport
 source .env
